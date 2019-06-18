7415017dd59f5381f77db8b2823a34002b235671
Muzamil Bukhari <46882812+Muzamilbukhari@users.noreply.github.com>
2019-06-18 13:42:37 +0500

    Update SDK url

--- src/MessengerPluginWrapper.js
+++ src/MessengerPluginWrapper.js
@@ -2,7 +2,7 @@ import makeAsyncScriptLoader from 'react-async-script';
 
 import MessengerPlugin from './MessengerPlugin';
 
-const URL = '//connect.facebook.net/en_US/sdk.js';
+const URL = '//connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
 
 export default makeAsyncScriptLoader(MessengerPlugin, URL, {
     globalName: 'FB'
